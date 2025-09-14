097dbba4fc4db8fcc79c3fd2ac8fbafc4a32ef69
Ann <annkarokiadmissions@gmail.com>
2025-09-14 20:47:33 +0300

    fix import

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
 from .models import Book
+from .forms import ExampleForm
 
 def book_list(request):
     books = Book.objects.all()
