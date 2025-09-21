03392bd52a93c5e3ce40573e19fed37f13555151
Ann <annkarokiadmissions@gmail.com>
2025-09-21 23:34:24 +0300

    feat: implement drf authentication

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import BookViewSet
 from .views import BookList
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
 router.register(r'books', BookViewSet, basename='book')
@@ -9,4 +10,6 @@ router.register(r'books', BookViewSet, basename='book')
 urlpatterns = [
     path('', include(router.urls)),
     path('books/', BookList.as_view(), name='book-list'),
+    path('auth/token/', obtain_auth_token, name='api_token_auth'),
+    
 ]
