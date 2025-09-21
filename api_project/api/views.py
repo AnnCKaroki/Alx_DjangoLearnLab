f7832b2c8603a7b9c936854f99a6295a63b1a6f8
Ann <annkarokiadmissions@gmail.com>
2025-09-21 23:20:16 +0300

    feat: add api endpoints

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework import permissions
 
-# Create your views here.
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
