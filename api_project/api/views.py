96bf7b1abcf6852c30923b74c656a4d76e07002f
Ann <annkarokiadmissions@gmail.com>
2025-09-21 23:23:28 +0300

    feat:  add booklist view

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,9 +3,13 @@ from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import permissions
+from rest_framework import generics
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
